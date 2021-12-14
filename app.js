c33724f6e5638fb05e217d065024fa4a0b4f34d7
Praj78 <prajwal.yeri@gmail.com>
2021-12-14 10:00:04 +0530

    some images removed

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     
     const cardArray = [
-      {
-        name: 'fries',
-        img: 'images/fries.png'
-      },
-      {
-        name: 'cheeseburger',
-        img: 'images/cheeseburger.png'
-      },
-      {
-        name: 'ice-cream',
-        img: 'images/ice-cream.png'
-      },
-      {
-        name: 'pizza',
-        img: 'images/pizza.png'
-      },
-      {
-        name: 'milkshake',
-        img: 'images/milkshake.png'
-      },
-      {
-        name: 'hotdog',
-        img: 'images/hotdog.png'
-      },
+     
+      
       {
         name: 'fries',
         img: 'images/fries.png'
